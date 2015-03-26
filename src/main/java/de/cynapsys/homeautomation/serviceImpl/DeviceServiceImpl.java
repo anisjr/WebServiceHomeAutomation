827842a230d43edf891a04e2f532935aed94edfe
mouadh <mouadh@myPC>
2015-03-26 13:53:56 +0100

    Devices webService final

--- src/main/java/de/cynapsys/homeautomation/serviceImpl/DeviceServiceImpl.java
+++ src/main/java/de/cynapsys/homeautomation/serviceImpl/DeviceServiceImpl.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.cynapsys.homeautomation.serviceImpl;
 
 import de.cynapsys.homeautomation.entity.Device;
@@ -20,7 +19,7 @@ import javax.ws.rs.Produces;
  * @author mouadh
  */
 @Path("/deviceservice")
-public class DeviceServiceImpl implements DevicesService{
+public class DeviceServiceImpl implements DevicesService {
 
     @Override
     @GET
@@ -28,41 +27,46 @@ public class DeviceServiceImpl implements DevicesService{
     @Produces("application/json")
     public List<Device> getAllDevices() {
         ArrayList<Device> listdevices = new ArrayList<>();
-        
-        Device d1 = new Device(1001L,"Device1","Device1 Description",00);
-        Device d2 = new Device(1002L,"Device2","Device2 Description",50);
-        Device d3 = new Device(1003L,"Device3","Device3 Description",80);
-        
-        listdevices.add(d3);listdevices.add(d2);listdevices.add(d1);
+
+        Device d1 = new Device(1001L, "Device1", "Device1 Description", 00);
+        Device d2 = new Device(1002L, "Device2", "Device2 Description", 50);
+        Device d3 = new Device(1003L, "Device3", "Device3 Description", 80);
+
+        listdevices.add(d3);
+        listdevices.add(d2);
+        listdevices.add(d1);
         System.out.println(listdevices);
         return listdevices;
     }
 
     @Override
     @GET
-    @Path("xx/{param}")
-    public boolean setValue(Long deviceID, int value) {
-        try{
+    @Path("/setValue/{varX}/{varY}")
+    public boolean setValue(@PathParam("varX") Long deviceID, @PathParam("varY") int value) {
+        try {
+            Device device = getDeviceByID(deviceID);
             device.setCurrentValue(value);
             return true;
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
 
-
-    public int getValue(Long deviceID) {
+    @Override
+    @GET
+    @Path("getValue/{param}")
+    public int getValue(@PathParam("param") Long deviceID) {
+        Device device = getDeviceByID(deviceID);
         System.out.println(device.getCurrentValue());
         return device.getCurrentValue();
     }
 
     @Override
     @GET
-    @Path("xx/{param}")
-    public Device getDeviceByID(Long deviceID) {
+    @Path("getDeviceByID/{param}")
+    public Device getDeviceByID(@PathParam("param") Long deviceID) {
         Device device = new Device(deviceID, "devideByID", "deviceByIDDescription", 80);
         return device;
     }
-    
+
 }
